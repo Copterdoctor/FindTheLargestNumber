a55b40a6a53412c99519fa73c935282cab2e3c62
CoptorDcotor <Jordananders@me.com>
2018-03-15 19:29:09 -0700

    added todo

--- ReturnHighestNumber/main.m
+++ ReturnHighestNumber/main.m
@@ -16,18 +16,21 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        //Creating a random array < 100 objects with random numbers < 1,000,000,000 in value
         NSMutableArray *randomArray = [[NSMutableArray alloc]init];
-        
         int randomLengthOfArray = arc4random_uniform(100);
-        
         for (int i = 0; i < randomLengthOfArray; i++) {
             int n = arc4random_uniform(1000000000);
             [randomArray addObject:[NSNumber numberWithInt:n]];
         }
-
         NSLog(@"The length of this array is %ld",[randomArray count]);
         NSLog(@"%@",randomArray);
+        
+        //Create object from custom class FindLargestNumber
+        //TODO Init object with highest number instead of having to call with function
         FindLargestNumber *x = [[FindLargestNumber alloc]init];
+        
+        //NSLog highest number from array passed to function largestNumberInArray
         NSLog(@"The largest number in this array is %@",[x largestNumberInArray:randomArray]);
         
     }
