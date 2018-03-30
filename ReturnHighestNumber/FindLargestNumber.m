4aaa50c85ab48adc17ebd2a2580ef4e6547a20b9
CoptorDcotor <Jordananders@me.com>
2018-03-30 16:28:14 +1000

    Added fast enumeration for practice

--- ReturnHighestNumber/FindLargestNumber.m
+++ ReturnHighestNumber/FindLargestNumber.m
@@ -12,18 +12,24 @@
 
 +(NSNumber*) largestNumberInArray: (NSArray *) array{
 
-//    //NS Sorting Method
+    NSMutableArray *tempArray = [[NSMutableArray alloc]initWithObjects:@0, nil];
+
+    //NS Sorting Method
 //    NSSortDescriptor *sorter = [[NSSortDescriptor alloc]initWithKey:@"self" ascending:NO];
 //    NSArray *sortedArray = [array sortedArrayUsingDescriptors:[NSArray arrayWithObject:sorter]];
 //    NSNumber *n = [sortedArray objectAtIndex:0];
     
-//    Jordans manual method
-    NSMutableArray *tempArray = [[NSMutableArray alloc]initWithObjects:@0, nil];
-    
+    //For loop method
     for (int i = 0; i < [array count]; i++) {
         if (array[i] > tempArray[0])
             [tempArray replaceObjectAtIndex:0 withObject:array[i]];
     }
+    
+    //Fast Enumeration of array
+//    for (NSNumber *i in array){
+//        if (i > tempArray[0])
+//            [tempArray replaceObjectAtIndex:0 withObject:i];
+//    }
     return tempArray[0];
 }
 
