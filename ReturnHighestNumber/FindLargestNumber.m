a4b0b70ac5b2170fb055f5a16a98fe9818b79feb
CoptorDcotor <Jordananders@me.com>
2018-03-30 17:07:32 +1000

    Got rid of tempArray that was not needed with FOR loop.

--- ReturnHighestNumber/FindLargestNumber.m
+++ ReturnHighestNumber/FindLargestNumber.m
@@ -12,25 +12,29 @@
 
 +(NSNumber*) largestNumberInArray: (NSArray *) array{
 
-    NSMutableArray *tempArray = [[NSMutableArray alloc]initWithObjects:@0, nil];
-
     //NS Sorting Method
+//    NSMutableArray *tempArray = [[NSMutableArray alloc]initWithObjects:@0, nil];
 //    NSSortDescriptor *sorter = [[NSSortDescriptor alloc]initWithKey:@"self" ascending:NO];
 //    NSArray *sortedArray = [array sortedArrayUsingDescriptors:[NSArray arrayWithObject:sorter]];
 //    NSNumber *n = [sortedArray objectAtIndex:0];
     
+    NSNumber *highestNumber = 0;
     //For loop method
-    for (int i = 0; i < [array count]; i++) {
-        if (array[i] > tempArray[0])
-            [tempArray replaceObjectAtIndex:0 withObject:array[i]];
-    }
+//    for (int i = 0; i < [array count]; i++) {
+//        if (array[i] > highestNumber){
+//            highestNumber = array[i];
+//        }
+//
+//    }
     
     //Fast Enumeration of array
-//    for (NSNumber *i in array){
-//        if (i > tempArray[0])
-//            [tempArray replaceObjectAtIndex:0 withObject:i];
-//    }
-    return tempArray[0];
+    for (NSNumber *i in array){
+        if (i > highestNumber){
+            highestNumber = i;
+            
+        }
+    }
+    return highestNumber;
 }
 
 @end
